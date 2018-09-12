98bd5e7b9c524a06a93281d6f9559e4ab004af95
Andrew Lee <leeas@stolaf.edu>
2018-09-12 00:58:28 -0500

    ehh

--- main.go
+++ main.go
@@ -14,18 +14,19 @@ func main() {
 		fmt.Println(err.Error())
 	}
 	inputpin := rpio.Pin(27)
-	outputpin := rpio.Pin(17)
+	// outputpin := rpio.Pin(17)
 
 	inputpin.Input()
-	outputpin.Output()
+	// outputpin.Output()
 
 	for {
 		time.Sleep(1 * time.Second)
-		if inputpin.Read() == rpio.High {
-			fmt.Println("High")
-			outputpin.High()
-		} else if inputpin.Read() == rpio.Low {
-			outputpin.Low()
-		}
+		fmt.Println(inputpin.Read())
+		// if inputpin.Read() == rpio.High {
+		// fmt.Println("High")
+		// outputpin.High()
+		// } else if inputpin.Read() == rpio.Low {
+		// outputpin.Low()
+		// }
 	}
 }
