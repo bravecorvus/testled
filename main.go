edb91c4f6bfc9b43409a496fc3e102fc48c73940
Andrew Lee <leeas@stolaf.edu>
2018-09-12 01:06:26 -0500

    ehh

--- main.go
+++ main.go
@@ -7,26 +7,44 @@ import (
 	rpio "github.com/stianeikeland/go-rpio"
 )
 
+// func main() {
+// err := rpio.Open()
+// if err != nil {
+// fmt.Println("failed to rpio.Open()")
+// fmt.Println(err.Error())
+// }
+// inputpin := rpio.Pin(27)
+// outputpin := rpio.Pin(17)
+//
+// inputpin.Input()
+// outputpin.Output()
+//
+// for {
+// time.Sleep(1 * time.Second)
+// if inputpin.Read() == rpio.High {
+// fmt.Println("High")
+// outputpin.High()
+// } else if inputpin.Read() == rpio.Low {
+// fmt.Println("Low")
+// outputpin.Low()
+// }
+// }
+// }
+
 func main() {
 	err := rpio.Open()
 	if err != nil {
 		fmt.Println("failed to rpio.Open()")
 		fmt.Println(err.Error())
 	}
-	inputpin := rpio.Pin(27)
+	// inputpin := rpio.Pin(27)
 	outputpin := rpio.Pin(17)
 
-	inputpin.Input()
+	// inputpin.Input()
 	outputpin.Output()
 
 	for {
 		time.Sleep(1 * time.Second)
-		if inputpin.Read() == rpio.High {
-			fmt.Println("High")
-			outputpin.High()
-		} else if inputpin.Read() == rpio.Low {
-			fmt.Println("Low")
-			outputpin.Low()
-		}
+		outputpin.Toggle()
 	}
 }
