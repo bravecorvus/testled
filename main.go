2771afb835cc6717ba2c2d6e41f8fbfd6874c893
Andrew Lee <leeas@stolaf.edu>
2018-09-12 21:40:24 -0500

    ehh

--- main.go
+++ main.go
@@ -30,32 +30,33 @@ import (
 // outputpin.Low()
 // }
 // }
+//
 // }
 
 func main() {
 	err := rpio.Open()
+
 	if err != nil {
 		fmt.Println("failed to rpio.Open()")
 		fmt.Println(err.Error())
 	}
+
 	defer rpio.Close()
-	inputpin := rpio.Pin(27)
+	// inputpin := rpio.Pin(27)
 	outputpin := rpio.Pin(17)
 
-	inputpin.Input()
-	inputpin.PullUp()
-	inputpin.Detect(rpio.FallEdge) // enable falling edge event detection
+	// inputpin.Input()
 	outputpin.Output()
 
-	for i := 0; i < 2; {
-		if inputpin.EdgeDetected() { // check if event occured
-			fmt.Println("Motion detected")
-			outputpin.High()
-			i++
-		} else {
-			outputpin.Low()
-		}
-		time.Sleep(time.Second / 2)
+	for {
+		time.Sleep(1 * time.Second)
+		// if inputpin.Read() == rpio.High {
+		// fmt.Println("High")
+		outputpin.Toggle()
+		// } else if inputpin.Read() == rpio.Low {
+		// fmt.Println("Low")
+		// outputpin.Low()
+		// }
 	}
-	inputpin.Detect(rpio.NoEdge) // disable edge event detection
+
 }
