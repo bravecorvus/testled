3fd11d308655866d1faae727737c1d4e1aa578bf
Andrew Lee <leeas@stolaf.edu>
2018-09-12 21:53:09 -0500

    ehh

--- test.py
+++ test.py
@@ -1,9 +1,21 @@
-from gpiozero import MotionSensor
 import RPi.GPIO as GPIO
+import time
 
-pir = MotionSensor(27)
+GPIO.setmode(GPIO.BCM)
 
-while True:
-	pir.wait_for_motion()
-	print("You moved")
-	pir.wait_for_no_motion()
+GPIO.setup(27, GPIO.IN) #PIR
+GPIO.setup(17, GPIO.OUT) #BUzzer
+
+try:
+    time.sleep(2) # to stabilize sensor
+    while True:
+        if GPIO.input(27):
+            GPIO.output(17, True)
+            time.sleep(0.5) #Buzzer turns on for 0.5 sec
+            GPIO.output(17, False)
+            print("Motion Detected...")
+            time.sleep(5) #to avoid multiple detection
+        time.sleep(0.1) #loop delay, should be less than detection delay
+
+except:
+    GPIO.cleanup()
