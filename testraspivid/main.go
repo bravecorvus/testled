824b6c9a7db54f98049d50e3d291e1e09c8af0fd
Andrew Lee <leeas@stolaf.edu>
2018-09-15 00:42:28 -0500

    ehh

--- testraspivid/main.go
+++ testraspivid/main.go
@@ -7,18 +7,26 @@ import (
 )
 
 func raspivid(output string, endsignal chan int) {
-	command := exec.Command("raspivid", "-o", "video.h264", "-t", "1000000000")
-	starterr := command.Start()
+	raspivid := exec.Command("raspivid", "-o", output+".h264", "-t", "1000000000")
+	starterr := raspivid.Start()
 	if starterr != nil {
 		fmt.Println("Can't start raspivid")
 		fmt.Println(starterr.Error())
 	}
 	_ = endsignal
-	killerr := command.Process.Kill()
+	killerr := raspivid.Process.Kill()
 	if killerr != nil {
 		fmt.Println("Can't kill raspivid")
 		fmt.Println(killerr.Error())
 	}
+
+	MP4Box := exec.Command("MP4Box", "-add", output+".h264", output+".mp4")
+	_, killerr = MP4Box.Output()
+	if killerr != nil {
+		fmt.Println("Can't kill MP$Box")
+		fmt.Println(killerr.Error())
+	}
+
 }
 
 func main() {
