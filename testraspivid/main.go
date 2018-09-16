48ac569a6881044709a6345ddc8af31e1669027e
Andrew Lee <leeas@stolaf.edu>
2018-09-16 14:47:34 -0500

    ehh

--- testraspivid/main.go
+++ testraspivid/main.go
@@ -7,27 +7,21 @@ import (
 	"time"
 )
 
-func startRecord(command exec.Cmd, output string) {
-	starterr := command.Start()
+func main() {
+	fmt.Println(os.Args[1])
+	raspivid := exec.Command("raspivid", "-o", os.Args[1]+".h264", "-t", "1000000000")
+
+	starterr := raspivid.Start()
 	if starterr != nil {
 		fmt.Println("Can't start raspivid")
 		fmt.Println(starterr.Error())
 	}
 
-}
+	time.Sleep(10)
 
-func stopRecord(command exec.Cmd) {
-	killerr := command.Process.Kill()
+	killerr := raspivid.Process.Kill()
 	if killerr != nil {
 		fmt.Println("Can't kill raspivid")
 		fmt.Println(killerr)
 	}
-}
-
-func main() {
-	fmt.Println(os.Args[1])
-	raspivid := exec.Command("raspivid", "-o", os.Args[1]+".h264", "-t", "1000000000")
-	startRecord(*raspivid, "video2.h264")
-	time.Sleep(10)
-	stopRecord(*raspivid)
 }
