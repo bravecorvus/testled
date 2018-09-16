89c047bf6daba3ba5a7db38f8de03c3856a2aff6
Andrew Lee <leeas@stolaf.edu>
2018-09-16 15:19:06 -0500

    ehh

--- testraspivid/main.go
+++ testraspivid/main.go
@@ -11,7 +11,7 @@ var (
 	raspivid *exec.Cmd
 )
 
-func startRecord(command exec.Cmd) {
+func startRecord(command *exec.Cmd) {
 	starterr := command.Start()
 	if starterr != nil {
 		fmt.Println("Can't start raspivid")
@@ -19,7 +19,7 @@ func startRecord(command exec.Cmd) {
 	}
 }
 
-func stopRecord(command exec.Cmd) {
+func stopRecord(command *exec.Cmd) {
 	killerr := command.Process.Kill()
 	if killerr != nil {
 		fmt.Println("Can't kill raspivid")
@@ -29,8 +29,8 @@ func stopRecord(command exec.Cmd) {
 
 func main() {
 	raspivid = exec.Command("raspivid", "-o", os.Args[1]+".h264", "-t", "1000000000")
-	startRecord(*raspivid)
+	startRecord(raspivid)
 	time.Sleep(10)
-	stopRecord(*raspivid)
+	stopRecord(raspivid)
 
 }
