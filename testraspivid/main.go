d53c756e9e7a84edf56b499aa8c32bd51c3f7b66
Andrew Lee <leeas@stolaf.edu>
2018-09-16 15:13:06 -0500

    ehh

--- testraspivid/main.go
+++ testraspivid/main.go
@@ -2,21 +2,39 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
+	"time"
 )
 
-func main() {
-	raspivid := exec.Command("raspivid", "-o", "video.h264", "-t", "1000000000")
+var (
+	raspivid *exec.Cmd
+)
 
-	starterr := raspivid.Start()
+func startRecord(command exec.Cmd, output string) {
+	starterr := command.Start()
 	if starterr != nil {
 		fmt.Println("Can't start raspivid")
 		fmt.Println(starterr.Error())
 	}
 
-	killerr := raspivid.Process.Kill()
+}
+
+func stopRecord(command exec.Cmd) {
+	killerr := command.Process.Kill()
 	if killerr != nil {
 		fmt.Println("Can't kill raspivid")
 		fmt.Println(killerr)
 	}
+}
+
+func init() {
+}
+
+func main() {
+	raspivid = exec.Command("raspivid", "-o", os.Args[1]+".h264", "-t", "1000000000")
+	startRecord(*raspivid, "video2.h264")
+	time.Sleep(10)
+	stopRecord(*raspivid)
+
 }
